57cea94b6d1fdbb2945b6540140972a0de5cf801
Diogo Ferreira <diogoveigaferreira@hotmail.com>
2026-01-28 16:10:28 -0300

    feat: adicionando autenticacao

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv';
+import { authMiddleware } from './middleware/auth.middleware.js'
+dotenv.config();
+
 const app = express()
-const port = 3000
+const port = process.env.PORT;
+
+app.use(express.json())
+app.use(authMiddleware);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+export default app;
